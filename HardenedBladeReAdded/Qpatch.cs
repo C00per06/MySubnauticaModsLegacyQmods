a5ac8a353f9e6283d0ceaaa20fc10479b3262bfb
c3988 <c3988@DESKTOP-KV87GIR>
2023-02-23 11:55:38 -0500

    Add project files.

---
+++ HardenedBladeReAdded/Qpatch.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using SMLHelper.V2.Interfaces;
+using SMLHelper.V2.MonoBehaviours;
+using SMLHelper.V2.Options;
+using UnityEngine;
+
+namespace DiamondBlade1
+{
+	[QModCore]
+	public class Qpatch
+	{
+		[QModPatch]
+		public static void Patch()
+		{
+			TechType techType = TechTypeHandler.AddTechType("DiamondBlade1", "Hardened Blade", "Diamond-hardened blade delivers higher damage.", SpriteManager.Get(TechType.DiamondBlade));
+			TechType techType2 = TechTypeHandler.AddTechType("DiamondBlade2", "Hardened Blade", "Diamond-hardened blade delivers higher damage.", SpriteManager.Get(TechType.DiamondBlade));
+			TechType techType3 = TechTypeHandler.AddTechType("DiamondBlade3", "Thermoblade", "Cooks and sterilizes small organisms for immediate consumption.", SpriteManager.Get(TechType.HeatBlade));
+			TechData techData = new TechData();
+			TechData techData2 = new TechData();
+			TechData techData3 = new TechData();
+
+
+			techData = new TechData
+			{
+				craftAmount = 0,
+				Ingredients = new List<Ingredient>
+				{
+					new Ingredient(TechType.Silicone, 1),
+					new Ingredient(TechType.Titanium, 1),
+					new Ingredient(TechType.Diamond, 1)
+				},
+				LinkedItems = new List<TechType>
+				{
+					TechType.DiamondBlade
+				}
+			};
+			CraftDataHandler.SetTechData(techType, techData);
+			CraftTreeHandler.AddCraftingNode(CraftTree.Type.Fabricator, techType, new string[]
+			{
+				"Personal",
+				"Tools"
+			});
+			techData2 = new TechData
+			{
+				craftAmount = 0,
+				Ingredients = new List<Ingredient>
+				{
+					new Ingredient(TechType.Knife, 1),
+					new Ingredient(TechType.Diamond, 1)
+				},
+				LinkedItems = new List<TechType>
+				{
+					TechType.DiamondBlade
+				}
+			};
+			CraftDataHandler.SetTechData(techType2, techData2);
+			CraftTreeHandler.AddCraftingNode(CraftTree.Type.Workbench, techType2, new string[]
+			{
+				"KnifeMenu"
+			});
+			techData3 = new TechData
+			{
+				craftAmount = 0,
+				Ingredients = new List<Ingredient>
+				{
+					new Ingredient(TechType.DiamondBlade, 1),
+					new Ingredient(TechType.Battery, 1)
+				},
+				LinkedItems = new List<TechType>
+				{
+					TechType.HeatBlade
+				}
+			};
+			CraftDataHandler.SetTechData(techType3, techData3);
+			CraftTreeHandler.AddCraftingNode(CraftTree.Type.Workbench, techType3, new string[]
+			{
+				"KnifeMenu"
+			});
+		}
+	}
+}
---
+++ HardenedBladeReAddedReduxBepInEx/HardenedBlade1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using SMLHelper;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+
+namespace HardenedBladeReAddedReduxBepInEx
+{
+    internal class HardenedBlade1 : Craftable
+    {
+        public HardenedBlade1() : base("HardenedBlade1", "Hardened Blade", "Diamond-hardened blade delivers higher damage.")
+    }
+}
